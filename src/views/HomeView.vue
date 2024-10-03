4868a89baac66502edfa20a758a74889a2eae081
mehdi <mehdi.0.saadi@gmail.com>
2024-10-03 17:23:45 +0330

    Use NavigationButton component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import ApplicationIcon from "@/components/icons/ApplicationIcon.vue";
+import LoginIcon from "@/components/icons/LoginIcon.vue";
+import NavigationButton from "@/components/buttons/NavigationButton.vue";
 import SearchIcon from "@/components/icons/SearchIcon.vue";
 import ShoppingCartIcon from "@/components/icons/ShoppingCartIcon.vue";
-import LoginIcon from "@/components/icons/LoginIcon.vue";
 
 </script>
 
@@ -19,20 +20,15 @@ import LoginIcon from "@/components/icons/LoginIcon.vue";
                 <SearchIcon class="min-w-6 max-w-6 me-6" />
             </div>
             <div class="flex items-center space-x-4 rtl:space-x-reverse">
-                <a
-                    class="flex items-center space-x-1 rtl:space-x-reverse text-sm py-2 px-4"
-                    href="#"
-                >
+                <NavigationButton href="#" >
                     <LoginIcon class="size-5" />
                     <span>ورود | ثبت نام</span>
-                </a>
-                <a
-                    class="flex items-center space-x-1 rtl:space-x-reverse text-sm py-2 px-4"
-                    href="#"
-                >
+                </NavigationButton>
+
+                <NavigationButton href="#" >
                     <ShoppingCartIcon class="size-6" />
                     <span>سبد خرید</span>
-                </a>
+                </NavigationButton>
             </div>
         </header>
     </div>
