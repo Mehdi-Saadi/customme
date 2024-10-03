9c03724a24fe1adb41079a3baac6dffe815b6e17
mehdi <mehdi.0.saadi@gmail.com>
2024-10-03 19:33:10 +0330

    Remove unused code

--- src/views/partials/FooterComponent.vue
+++ src/views/partials/FooterComponent.vue
@@ -3,6 +3,33 @@ import FacebookIcon from '@/components/icons/FacebookIcon.vue';
 import InstagramIcon from '@/components/icons/InstagramIcon.vue';
 import PinterestIcon from '@/components/icons/PinterestIcon.vue';
 import YoutubeIcon from '@/components/icons/YoutubeIcon.vue';
+
+const socialLinks = [
+    {
+        name: 'instagram',
+        icon: InstagramIcon,
+        iconClass: 'size-6',
+        link: '#',
+    },
+    {
+        name: 'facebook',
+        icon: FacebookIcon,
+        iconClass: 'size-6',
+        link: '#',
+    },
+    {
+        name: 'pinterest',
+        icon: PinterestIcon,
+        iconClass: 'size-6',
+        link: '#',
+    },
+    {
+        name: 'youtube',
+        icon: YoutubeIcon,
+        iconClass: 'w-8 h-5',
+        link: '#',
+    },
+];
 </script>
 
 <template>
@@ -31,12 +58,20 @@ import YoutubeIcon from '@/components/icons/YoutubeIcon.vue';
             <!-- social links -->
             <div class="col-span-1 flex items-center justify-center mt-10 mb-16">
                 <div class="flex flex-col items-center space-y-5">
-                    <span class="text-sm text-white">ما را در شبکه های اجتماعی دنبال کنید</span>
+                    <span class="text-sm text-white">
+                        ما را در شبکه های اجتماعی دنبال کنید
+                    </span>
                     <div class="flex items-center space-x-6 rtl:space-x-reverse">
-                        <InstagramIcon class="size-6" />
-                        <FacebookIcon class="size-6" />
-                        <PinterestIcon class="size-6" />
-                        <YoutubeIcon class="w-8 h-5" />
+                        <a
+                            v-for="socialLink in socialLinks"
+                            :key="socialLink.name"
+                            :href="socialLink.link"
+                        >
+                            <component
+                                :is="socialLink.icon"
+                                :class="socialLink.iconClass"
+                            />
+                        </a>
                     </div>
                 </div>
             </div>
