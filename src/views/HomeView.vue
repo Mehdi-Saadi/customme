816f461098c761ccbef6c972a989109b8bd4f0b9
mehdi <mehdi.0.saadi@gmail.com>
2024-10-03 17:34:24 +0330

    Use HeaderComponent

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,36 +1,9 @@
 <script setup lang="ts">
-import ApplicationIcon from '@/components/icons/ApplicationIcon.vue';
-import LoginIcon from '@/components/icons/LoginIcon.vue';
-import NavigationButton from '@/components/buttons/NavigationButton.vue';
-import SearchIcon from '@/components/icons/SearchIcon.vue';
-import ShoppingCartIcon from '@/components/icons/ShoppingCartIcon.vue';
+import HeaderComponent from "@/views/partials/HeaderComponent.vue";
 </script>
 
 <template>
-    <div class="max-w-screen-xl mx-auto">
-        <header class="flex items-center space-x-16 rtl:space-x-reverse py-7">
-            <ApplicationIcon />
-            <div
-                class="flex grow items-center h-12 rounded-2xl border border-gray-300 focus-within:border-gray-400 p-0.5"
-            >
-                <input
-                    autocomplete="off"
-                    class="size-full rounded-2xl focus:ring-0 border-none px-6"
-                    type="text"
-                />
-                <SearchIcon class="min-w-6 max-w-6 me-6" />
-            </div>
-            <div class="flex items-center space-x-4 rtl:space-x-reverse">
-                <NavigationButton href="#">
-                    <LoginIcon class="size-5" />
-                    <span>ورود | ثبت نام</span>
-                </NavigationButton>
-
-                <NavigationButton href="#">
-                    <ShoppingCartIcon class="size-6" />
-                    <span>سبد خرید</span>
-                </NavigationButton>
-            </div>
-        </header>
+    <div class="max-w-screen-xl mx-auto px-6">
+        <HeaderComponent />
     </div>
 </template>
