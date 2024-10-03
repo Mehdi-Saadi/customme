61cb98dec8e78d8d860c0dfe0f9276db099833e0
mehdi <mehdi.0.saadi@gmail.com>
2024-10-03 17:25:15 +0330

    Use format

--- src/components/buttons/NavigationButton.vue
+++ src/components/buttons/NavigationButton.vue
@@ -1,7 +1,5 @@
 <template>
-    <a
-        class="flex items-center space-x-1 rtl:space-x-reverse text-sm py-2 px-4"
-    >
+    <a class="flex items-center space-x-1 rtl:space-x-reverse text-sm py-2 px-4">
         <slot />
     </a>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,18 @@
 <script setup lang="ts">
-import ApplicationIcon from "@/components/icons/ApplicationIcon.vue";
-import LoginIcon from "@/components/icons/LoginIcon.vue";
-import NavigationButton from "@/components/buttons/NavigationButton.vue";
-import SearchIcon from "@/components/icons/SearchIcon.vue";
-import ShoppingCartIcon from "@/components/icons/ShoppingCartIcon.vue";
-
+import ApplicationIcon from '@/components/icons/ApplicationIcon.vue';
+import LoginIcon from '@/components/icons/LoginIcon.vue';
+import NavigationButton from '@/components/buttons/NavigationButton.vue';
+import SearchIcon from '@/components/icons/SearchIcon.vue';
+import ShoppingCartIcon from '@/components/icons/ShoppingCartIcon.vue';
 </script>
 
 <template>
     <div class="max-w-screen-xl mx-auto">
         <header class="flex items-center space-x-16 rtl:space-x-reverse py-7">
             <ApplicationIcon />
-            <div class="flex grow items-center h-12 rounded-2xl border border-gray-300 focus-within:border-gray-400 p-0.5">
+            <div
+                class="flex grow items-center h-12 rounded-2xl border border-gray-300 focus-within:border-gray-400 p-0.5"
+            >
                 <input
                     autocomplete="off"
                     class="size-full rounded-2xl focus:ring-0 border-none px-6"
@@ -20,12 +21,12 @@ import ShoppingCartIcon from "@/components/icons/ShoppingCartIcon.vue";
                 <SearchIcon class="min-w-6 max-w-6 me-6" />
             </div>
             <div class="flex items-center space-x-4 rtl:space-x-reverse">
-                <NavigationButton href="#" >
+                <NavigationButton href="#">
                     <LoginIcon class="size-5" />
                     <span>ورود | ثبت نام</span>
                 </NavigationButton>
 
-                <NavigationButton href="#" >
+                <NavigationButton href="#">
                     <ShoppingCartIcon class="size-6" />
                     <span>سبد خرید</span>
                 </NavigationButton>
