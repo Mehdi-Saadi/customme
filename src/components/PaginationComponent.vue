a42713fac1c1243b7ab704edebe42696e2192e50
mehdi <mehdi.0.saadi@gmail.com>
2024-10-05 14:26:08 +0330

    Use PaginationComponent

---
+++ src/components/PaginationComponent.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import CircleArrowRightIcon from "@/components/icons/CircleArrowRightIcon.vue";
+import CircleArrowLeftIcon from "@/components/icons/CircleArrowLeftIcon.vue";
+import useProductStore from "@/stores/product";
+
+const { nextPage, previousPage} = useProductStore();
+</script>
+
+<template>
+    <div class="w-full flex items-center mt-10 h-14 px-28 space-x-3 rtl:space-x-reverse">
+        <CircleArrowRightIcon
+            @click="previousPage()"
+            class="size-6 cursor-pointer"
+            title="قبلی"
+        />
+        <div class="grow h-full border rounded-3xl py-3 px-9"></div>
+        <CircleArrowLeftIcon
+            @click="nextPage()"
+            class="size-6 cursor-pointer"
+            title="بعدی"
+        />
+    </div>
+</template>
--- src/views/ProductListView.vue
+++ src/views/ProductListView.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-import CircleArrowLeftIcon from '@/components/icons/CircleArrowLeftIcon.vue';
-import CircleArrowRightIcon from '@/components/icons/CircleArrowRightIcon.vue';
+import PaginationComponent from "@/components/PaginationComponent.vue";
 import ProductCard from '@/components/product/ProductCard.vue';
 import StoreLayout from '@/layouts/StoreLayout.vue';
 import useProductStore from '@/stores/product';
 
-const { fetchProducts, nextPage, previousPage, getImage } = useProductStore();
+const { fetchProducts, getImage } = useProductStore();
 
 fetchProducts();
 </script>
@@ -23,19 +22,7 @@ fetchProducts();
                     :price="product.attributes.price"
                 />
             </div>
-            <div class="w-full flex items-center mt-10 h-14 px-28 space-x-3 rtl:space-x-reverse">
-                <CircleArrowRightIcon
-                    @click="previousPage()"
-                    class="size-6 cursor-pointer"
-                    title="قبلی"
-                />
-                <div class="grow h-full border rounded-3xl py-3 px-9"></div>
-                <CircleArrowLeftIcon
-                    @click="nextPage()"
-                    class="size-6 cursor-pointer"
-                    title="بعدی"
-                />
-            </div>
+            <PaginationComponent />
         </template>
         <span
             v-else
