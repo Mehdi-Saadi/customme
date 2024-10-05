0f8defb5cb1b7dd65f4108918052fc281b31a4e3
mehdi <mehdi.0.saadi@gmail.com>
2024-10-05 19:31:29 +0330

    Update url

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
-            path: '/',
+            path: '/products',
             name: 'product.list',
             component: ProductListView,
         },
