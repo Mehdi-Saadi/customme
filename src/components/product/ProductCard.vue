1dbf220699eb54a7403828b9eaa1d5a61cfb889e
mehdi <mehdi.0.saadi@gmail.com>
2024-10-12 12:31:49 +0330

    Add "Add to card" button

--- src/components/product/ProductCard.vue
+++ src/components/product/ProductCard.vue
@@ -8,7 +8,7 @@ defineProps<{
 
 <template>
     <div
-        class="border border-gray-300 rounded-2xl w-full h-[26.875rem] p-4 flex flex-col items-center"
+        class="border border-gray-300 rounded-2xl w-full h-[30rem] p-4 flex flex-col items-center"
     >
         <!-- img -->
         <img
@@ -29,5 +29,11 @@ defineProps<{
         ></p>
         <!-- price -->
         <span class="w-full font-bold text-xl mt-auto text-end">{{ product.price }} تومان</span>
+        <button
+            class="w-full border border-rose-750 py-2 mt-3 text-rose-750 text-sm rounded-lg hover:text-rose-950 hover:border-rose-950 transition duration-300"
+            type="button"
+        >
+            <span>افزودن به سبد خرید</span>
+        </button>
     </div>
 </template>
