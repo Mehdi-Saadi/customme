962a5f82b43b2277a00acebedba68ddbc6b83939
mehdi <mehdi.0.saadi@gmail.com>
2024-10-13 16:44:54 +0330

    Implement discount code section in sidebar

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -41,7 +41,33 @@ const productStore = useProductStore();
                         :cart-item="cartItem"
                     />
                 </div>
-
+                <!-- sidebar -->
+                <div class="col-span-1 flex flex-col items-center px-3">
+                    <!-- prices -->
+                    <div class="w-full border rounded-2xl p-6 space-y-4">
+                        <!-- discount code -->
+                        <div class="w-full flex flex-col items-center border-b px-2 pt-2 pb-4 space-y-3">
+                            <span class="w-full text-sm">
+                                کد تخفیف
+                            </span>
+                            <p class="text-2xs text-gray-600">
+                                کد تخفیف کد معرف کارت هدیه خود را در زیر وارد کرده و دکمه ثبت رو بزنید تا در صورت داشتن اعتبار به سفارش شما اعمال شود
+                            </p>
+                            <div class="w-full flex items-center space-x-2 rtl:space-x-reverse">
+                                <input
+                                    class="rounded-lg max-w-[14.5rem] border-gray-300 focus:border-gray-400 focus:ring-0 h-10"
+                                    type="text"
+                                />
+                                <button
+                                    class="border border-rose-750 text-rose-750 px-7 py-2 rounded-lg text-sm hover:border-rose-950 hover:text-rose-950"
+                                    type="button"
+                                >
+                                    ثبت
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </StoreLayout>
