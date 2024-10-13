908cd4f84359e35bdcb41f0aee66b467198d4a86
mehdi <mehdi.0.saadi@gmail.com>
2024-10-13 15:42:05 +0330

    Implement product count functionality

--- src/components/product/CartItemCard.vue
+++ src/components/product/CartItemCard.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
 import CloseSquareIcon from '@/assets/icons/close-square-icon.svg';
+import PlusSmallIcon from "@/assets/icons/plus-small-icon.svg";
+import MinusSmallIcon from "@/assets/icons/minus-small-icon.svg";
+import useProductStore from "@/stores/product";
+import { toPersianNums } from "@/scripts/helpers";
+import { computed } from "vue";
 
-defineProps<{
+const props = defineProps<{
     cartItem: ShoppingCartItem;
 }>();
+
+const { addToCart, removeFromCart } = useProductStore();
+
+const countOfProductInShoppingCart = computed(() => {
+    const item = useProductStore().shoppingCart[props.cartItem.product.id];
+    return item ? item.count : 0;
+});
 </script>
 
 <template>
@@ -20,11 +32,23 @@ defineProps<{
                 {{ cartItem.product.attributes.name }}
             </span>
             <span>
-                {{ cartItem.product.attributes.price }} تومان
+                {{ toPersianNums(cartItem.product.attributes.price) }} تومان
             </span>
         </div>
-        <div>
-            buttons
+        <div
+            class="flex items-center justify-between border text-sm rounded-lg select-none"
+        >
+            <PlusSmallIcon
+                @click="addToCart(cartItem.product)"
+                class="size-8 cursor-pointer border-e"
+            />
+            <span class="px-3">
+                {{ toPersianNums(String(countOfProductInShoppingCart)) }}
+            </span>
+            <MinusSmallIcon
+                @click="removeFromCart(cartItem.product)"
+                class="size-8 cursor-pointer border-s"
+            />
         </div>
     </div>
 </template>
