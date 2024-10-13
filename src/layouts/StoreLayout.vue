da3c7bc69dd74684a143bb5c33ad3497ccc268cf
mehdi <mehdi.0.saadi@gmail.com>
2024-10-13 13:52:04 +0330

    Use SortMenu and FilterMenu components in ProductListView page

--- src/layouts/StoreLayout.vue
+++ src/layouts/StoreLayout.vue
@@ -1,19 +1,12 @@
 <script setup lang="ts">
-import FilterMenu from '@/components/menus/FilterMenu.vue';
 import FooterComponent from '@/views/partials/FooterComponent.vue';
-import SortMenu from '@/components/menus/SortMenu.vue';
 import TopBarComponent from '@/views/partials/TopBarComponent.vue';
 </script>
 
 <template>
     <TopBarComponent />
     <div class="max-w-screen-xl mx-auto px-6 pt-56 flex items-start justify-start">
-        <FilterMenu />
-        <div class="w-full flex flex-col ps-6 pe-4">
-            <SortMenu />
-            <!-- content -->
-            <slot />
-        </div>
+        <slot />
     </div>
     <FooterComponent />
 </template>
--- src/views/ProductListView.vue
+++ src/views/ProductListView.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
+import FilterMenu from '@/components/menus/FilterMenu.vue';
 import PaginationComponent from '@/components/PaginationComponent.vue';
 import ProductCard from '@/components/product/ProductCard.vue';
+import SortMenu from '@/components/menus/SortMenu.vue';
 import StoreLayout from '@/layouts/StoreLayout.vue';
 import useProductStore from '@/stores/product';
 
@@ -11,21 +13,26 @@ setParametersAndFetchProducts();
 
 <template>
     <StoreLayout>
-        <template v-if="useProductStore().products.length">
-            <div class="w-full grid lg:grid-cols-3 grid-cols-2 gap-5 mt-6">
-                <ProductCard
-                    v-for="product in useProductStore().products"
-                    :key="product.id"
-                    :product="product"
-                />
-            </div>
-            <PaginationComponent />
-        </template>
-        <span
-            v-else
-            class="mx-auto mt-24 mb-40"
-        >
-            درحال بارگیری...
-        </span>
+        <FilterMenu />
+        <div class="w-full flex flex-col ps-6 pe-4">
+            <SortMenu />
+            <!-- content -->
+            <template v-if="useProductStore().products.length">
+                <div class="w-full grid lg:grid-cols-3 grid-cols-2 gap-5 mt-6">
+                    <ProductCard
+                        v-for="product in useProductStore().products"
+                        :key="product.id"
+                        :product="product"
+                    />
+                </div>
+                <PaginationComponent />
+            </template>
+            <span
+                v-else
+                class="mx-auto mt-24 mb-40"
+            >
+                درحال بارگیری...
+            </span>
+        </div>
     </StoreLayout>
 </template>
