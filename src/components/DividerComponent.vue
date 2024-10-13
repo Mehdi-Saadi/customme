0dd45bfcd8e18b08e099f1b7ff8cb6524d9ac3a6
mehdi <mehdi.0.saadi@gmail.com>
2024-10-13 14:37:24 +0330

    Add progress section

---
+++ src/components/DividerComponent.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="flex items-center">
+        <span class="size-2 bg-rose-750 rounded-full" />
+        <span class="w-28 border border-dashed border-rose-750" />
+        <span class="size-2 bg-rose-750 rounded-full" />
+    </div>
+</template>
--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,9 +1,31 @@
 <script setup lang="ts">
+import DividerComponent from '@/components/DividerComponent.vue';
+import DollarCircleArrowTopRightIcon from '@/assets/icons/dollar-circle-arrow-top-right-icon.svg';
+import LocationIcon from '@/assets/icons/location-icon.svg';
+import SoppingCartRoseIcon from '@/assets/icons/shopping-cart-rose-icon.svg';
 import StoreLayout from '@/layouts/StoreLayout.vue';
 </script>
 
 <template>
     <StoreLayout>
-        <span>something...</span>
+        <div class="w-full flex flex-col items-center">
+            <!-- progress icons -->
+            <div class="flex items-center text-rose-750 text-xs space-x-6 rtl:space-x-reverse">
+                <div class="flex flex-col items-center justify-center">
+                    <SoppingCartRoseIcon class="size-6" />
+                    <span>سبد خرید</span>
+                </div>
+                <DividerComponent class="opacity-50" />
+                <div class="flex flex-col items-center justify-center opacity-50">
+                    <LocationIcon class="size-6" />
+                    <span>اطلاعات آدرس</span>
+                </div>
+                <DividerComponent class="opacity-50" />
+                <div class="flex flex-col items-center justify-center opacity-50">
+                    <DollarCircleArrowTopRightIcon class="size-6" />
+                    <span>پرداخت</span>
+                </div>
+            </div>
+        </div>
     </StoreLayout>
 </template>
