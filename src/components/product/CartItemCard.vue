9b157e2a062607b3508486187f2c08a22dc96679
mehdi <mehdi.0.saadi@gmail.com>
2024-10-13 15:55:23 +0330

    Implement delivery time

--- src/components/product/CartItemCard.vue
+++ src/components/product/CartItemCard.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import CloseSquareIcon from '@/assets/icons/close-square-icon.svg';
-import PlusSmallIcon from '@/assets/icons/plus-small-icon.svg';
 import MinusSmallIcon from '@/assets/icons/minus-small-icon.svg';
+import PlusSmallIcon from '@/assets/icons/plus-small-icon.svg';
+import TruckFastIcon from '@/assets/icons/truck-fast-icon.svg';
 import useProductStore from '@/stores/product';
 import { toPersianNums } from '@/scripts/helpers';
 import { computed } from 'vue';
@@ -19,35 +20,41 @@ const countOfProductInShoppingCart = computed(() => {
 </script>
 
 <template>
-    <div class="flex items-center border-b">
-        <div class="grow flex items-center text-sm font-bold space-x-6 rtl:space-x-reverse">
-            <CloseSquareIcon
-                @click="clearFromCart(cartItem.product)"
-                class="size-6 cursor-pointer"
-            />
-            <img
-                :src="cartItem.product.attributes.image"
-                alt=""
-                class="size-32 rounded-lg"
-                loading="lazy"
-            />
-            <span>
-                {{ cartItem.product.attributes.name }}
-            </span>
-            <span> {{ toPersianNums(cartItem.product.attributes.price) }} تومان </span>
+    <div class="flex flex-col items-center border-b">
+        <div class="w-full flex items-center">
+            <div class="grow flex items-center text-sm font-bold space-x-6 rtl:space-x-reverse">
+                <CloseSquareIcon
+                    @click="clearFromCart(cartItem.product)"
+                    class="size-6 cursor-pointer"
+                />
+                <img
+                    :src="cartItem.product.attributes.image"
+                    alt=""
+                    class="size-32 rounded-lg"
+                    loading="lazy"
+                />
+                <span>
+                    {{ cartItem.product.attributes.name }}
+                </span>
+                <span> {{ toPersianNums(cartItem.product.attributes.price) }} تومان </span>
+            </div>
+            <div class="flex items-center justify-between border text-sm rounded-lg select-none">
+                <PlusSmallIcon
+                    @click="addToCart(cartItem.product)"
+                    class="size-8 cursor-pointer border-e"
+                />
+                <span class="px-3">
+                    {{ toPersianNums(String(countOfProductInShoppingCart)) }}
+                </span>
+                <MinusSmallIcon
+                    @click="removeFromCart(cartItem.product)"
+                    class="size-8 cursor-pointer border-s"
+                />
+            </div>
         </div>
-        <div class="flex items-center justify-between border text-sm rounded-lg select-none">
-            <PlusSmallIcon
-                @click="addToCart(cartItem.product)"
-                class="size-8 cursor-pointer border-e"
-            />
-            <span class="px-3">
-                {{ toPersianNums(String(countOfProductInShoppingCart)) }}
-            </span>
-            <MinusSmallIcon
-                @click="removeFromCart(cartItem.product)"
-                class="size-8 cursor-pointer border-s"
-            />
+        <div class="w-full flex items-center justify-end h-10 space-x-1 rtl:space-x-reverse">
+            <TruckFastIcon class="size-6" />
+            <span class="text-sm text-gray-600"> ارسال از ۳ روز آینده </span>
         </div>
     </div>
 </template>
