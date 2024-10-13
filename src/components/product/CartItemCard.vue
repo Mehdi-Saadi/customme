7fb587e184e62167ca873d70f0d46a4ed4741da2
mehdi <mehdi.0.saadi@gmail.com>
2024-10-13 15:45:06 +0330

    Implement delete product functionality

--- src/components/product/CartItemCard.vue
+++ src/components/product/CartItemCard.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
 import CloseSquareIcon from '@/assets/icons/close-square-icon.svg';
-import PlusSmallIcon from "@/assets/icons/plus-small-icon.svg";
-import MinusSmallIcon from "@/assets/icons/minus-small-icon.svg";
-import useProductStore from "@/stores/product";
-import { toPersianNums } from "@/scripts/helpers";
-import { computed } from "vue";
+import PlusSmallIcon from '@/assets/icons/plus-small-icon.svg';
+import MinusSmallIcon from '@/assets/icons/minus-small-icon.svg';
+import useProductStore from '@/stores/product';
+import { toPersianNums } from '@/scripts/helpers';
+import { computed } from 'vue';
 
 const props = defineProps<{
     cartItem: ShoppingCartItem;
 }>();
 
-const { addToCart, removeFromCart } = useProductStore();
+const { addToCart, removeFromCart, clearFromCart } = useProductStore();
 
 const countOfProductInShoppingCart = computed(() => {
     const item = useProductStore().shoppingCart[props.cartItem.product.id];
@@ -21,7 +21,10 @@ const countOfProductInShoppingCart = computed(() => {
 <template>
     <div class="flex items-center border-b">
         <div class="grow flex items-center text-sm font-bold space-x-6 rtl:space-x-reverse">
-            <CloseSquareIcon class="size-6" />
+            <CloseSquareIcon
+                @click="clearFromCart(cartItem.product)"
+                class="size-6 cursor-pointer"
+            />
             <img
                 :src="cartItem.product.attributes.image"
                 alt=""
@@ -31,13 +34,9 @@ const countOfProductInShoppingCart = computed(() => {
             <span>
                 {{ cartItem.product.attributes.name }}
             </span>
-            <span>
-                {{ toPersianNums(cartItem.product.attributes.price) }} تومان
-            </span>
+            <span> {{ toPersianNums(cartItem.product.attributes.price) }} تومان </span>
         </div>
-        <div
-            class="flex items-center justify-between border text-sm rounded-lg select-none"
-        >
+        <div class="flex items-center justify-between border text-sm rounded-lg select-none">
             <PlusSmallIcon
                 @click="addToCart(cartItem.product)"
                 class="size-8 cursor-pointer border-e"
